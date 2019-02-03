07d0d2e3b0dfbf334771268d82c85ec1ce428b76
Ryan Grunest <ryangrunest@Ryans-MacBook-Air.local>
2019-02-03 15:49:25 -0800

    rough draft for CSS, added some javascript

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,1 +1,22 @@
-console.log("helloworld");
+// Initialize Selectors
+// Reset Button
+var resetButton = document.querySelector("#reset");
+// Instructions Button
+var instructions = document.querySelector("#instructions");
+
+
+
+// Listener for reset button
+resetButton.addEventListener("click", function() {
+	location.reload();
+});
+
+// Listener for instructions button
+instructions.addEventListener("click", function() {
+	alert("write some instructions");
+});
+
+document.onkeydown = function() {
+	alert("key pressed");
+
+}
